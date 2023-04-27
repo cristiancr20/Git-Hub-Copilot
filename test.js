28fcbb8e04965a6d8c2fad86a084b00a3f6758f3
Alex vicente <alex.avj12@gmail.com>
2023-04-26 22:13:51 -0500

    Generando comentarios y codigo con Copilot

--- test.js
+++ test.js
@@ -1,8 +1,31 @@
+//Funcion para multiplicar dos numeros
+function multiply(a, b) {
+    //Se multiplican los dos numeros
+    return a * b;
+}
+
+
+//Funcion para dividir dos numeros
+function divide(a, b) {
+    //Se dividen los dos numeros
+    return a / b;
+}
+
 //Funcion para sumar dos numeros
 function add(a, b) {
+    //Se suman los dos numeros
     return a + b;
 }
- 
+
+
+//Funcion para restar dos numeros
+function subtract(a, b) {
+    //Se restan los dos numeros
+    return a - b;
+}
+
+console.log("La suma es :",add(50,3));
+console.log("La resta es :",add(25,3));
 
 
 
